d09d52ba0c235579efdea0dad3c5ff003540cd84
msyamrijal <msyamrijal@gmail.com>
2025-09-21 21:01:04 +0700

    Update admin UID in auth-admin.js for authentication checks

--- auth-admin.js
+++ auth-admin.js
@@ -4,7 +4,7 @@
 // Ganti nilai di bawah ini dengan User ID (UID) akun Anda dari Firebase Authentication.
 // Anda bisa menambahkan lebih dari satu UID di dalam array ini.
 // Contoh: const ADMIN_UIDS = ['uid-admin-satu', 'uid-admin-kedua'];
-const ADMIN_UIDS = ['GANTI_DENGAN_UID_ADMIN_ANDA'];
+const ADMIN_UIDS = ['v22ZHhLZi0deKCkIKEE5gtvRUN22'];
 
 /**
  * Memeriksa apakah pengguna yang diberikan adalah seorang admin.
